7b6c9e83cd0e4f4cf93e88e12ca09adfd8461e0e
Michał Łaszczewski <michal@laszczewski.com>
2021-11-07 10:00:22 +0100

    cleanup

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,70 +1,77 @@
-const path = require('path')
-const vuePlugin = require('@vitejs/plugin-vue')
+import path from 'path'
+import vuePlugin from '@vitejs/plugin-vue'
+import getPort from 'get-port'
+import { defineConfig } from 'vite'
 
-/**
- * @type {import('vite').UserConfig}
- */
-module.exports = {
-  plugins: [
-    vuePlugin({
-      template: {
-        compilerOptions: {
-          whitespace: "preserve",
+export default defineConfig(async ({ command, mode }) => {
+  return {
+    server: {
+      hmr: {
+        port: await getPort()
+      }
+    },
+    plugins: [
+      vuePlugin({
+        template: {
+          compilerOptions: {
+            whitespace: "preserve",
+          },
         },
-      },
-    })
-  ],
-  build: {
-    minify: false,
-    commonjsOptions: {
-      transformMixedEsModules: true
-    }
-  },
-  ssr: {
-    external: [
-      '@live-change/dao',
-      '@live-change/vue-api',
-      '@live-change/vue-api-session',
-      'debug',
-      '@vue/server-renderer',
-      'vite'
+      })
     ],
-    noExternal: [
-      'vue-meta',
-      '@live-change/vue3-components',
-      '@live-change/dao-vue3',
-      '@live-change/vue3-ssr'
-    ]
-  },
-  optimizeDeps: {
-    include: [
-      '@live-change/vue-api',
-      '@live-change/vue-api-session',
-      '@live-change/dao',
-      '@live-change/dao-sockjs',
-      '@live-change/dao-websocket',
-      'debug'
-    ]
-  },
+    build: {
+      minify: false,
+      commonjsOptions: {
+        transformMixedEsModules: true
+      }
+    },
+    ssr: {
+      external: [
+        '@live-change/dao',
+        '@live-change/vue-api',
+        '@live-change/vue-api-session',
+        'debug',
+        '@vue/server-renderer',
+        'vite'
+      ],
+      noExternal: [
+        'vue-meta',
+        '@live-change/vue3-components',
+        '@live-change/dao-vue3',
+        '@live-change/vue3-ssr'
+      ]
+    },
+    optimizeDeps: {
+      include: [
+        '@live-change/vue-api',
+        '@live-change/vue-api-session',
+        '@live-change/dao',
+        '@live-change/dao-sockjs',
+        '@live-change/dao-websocket',
+        'debug'
+      ]
+    },
 
-  resolve: {
-    alias: [
-      { find: 'debug', replacement: 'debug/src/browser.js' },
-      { find: 'universal-websocket-client', replacement: 'universal-websocket-client/browser.js' },
-      { find: 'sockjs-client', replacement: 'sockjs-client/dist/sockjs.min.js' }
-    ],
-  },
-  resolvers: [{
-    fileToRequest (filePath) {
-      console.log('@@@', filePath);
-      if (filePath.startsWith(srcPath)) {
-        return `/@/${path.relative(srcPath, filePath)}`;
-      }
+    resolve: {
+      alias: [
+        { find: 'debug', replacement: 'debug/src/browser.js' },
+        { find: 'universal-websocket-client', replacement: 'universal-websocket-client/browser.js' },
+        { find: 'sockjs-client', replacement: 'sockjs-client/dist/sockjs.min.js' }
+      ],
     },
-    requestToFile (publicPath) {
-      if (publicPath.startsWith('/@/')) {
-        return path.join(srcPath, publicPath.replace(/^\/@\//, ''));
-      }
-    },
-  }],
-}
+    resolvers: [{
+      fileToRequest (filePath) {
+        console.log('@@@', filePath);
+        if (filePath.startsWith(srcPath)) {
+          return `/@/${path.relative(srcPath, filePath)}`;
+        }
+      },
+      requestToFile (publicPath) {
+        if (publicPath.startsWith('/@/')) {
+          return path.join(srcPath, publicPath.replace(/^\/@\//, ''));
+        }
+      },
+    }],
+  }
+})
+
